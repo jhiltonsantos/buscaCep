54e027c53fbfa30097587dd31f24f8614d0da65e
Hilton Santos <jhilton930@gmail.com>
2023-08-13 02:26:32 -0300

    update nos testes dos hooks

--- __tests__/src/hooks/useVisibilidadeSenha.test.ts
+++ __tests__/src/hooks/useVisibilidadeSenha.test.ts
@@ -1,16 +1,16 @@
-import {renderHook, act} from '@testing-library/react-hooks';
-import {pegarVisibilidadeSenha} from '../../../src/hooks/useVisibilidadeSenha';
+import { renderHook, act } from '@testing-library/react-hooks';
+import { useVisibilidadeSenha } from '../../../src/hooks/useVisibilidadeSenha';
 
 describe('Hook pegarVisibilidadeSenha', () => {
   it('A senha deve estar visivel e o icone deve ser o mostrar', () => {
-    const {result} = renderHook(() => pegarVisibilidadeSenha());
+    const { result } = renderHook(() => useVisibilidadeSenha());
 
     expect(result.current.senhaVisibilidade).toBe(true);
     expect(result.current.iconeDireita).toBe('mostrar');
   });
 
   it('Chamar a função mudarVisibilidadeSenha para alterar a visiblidade da senha se tornando invisível e o ícone deve ser o enconder', () => {
-    const {result} = renderHook(() => pegarVisibilidadeSenha());
+    const { result } = renderHook(() => useVisibilidadeSenha());
 
     act(() => {
       result.current.mudarVisibilidadeSenha();
